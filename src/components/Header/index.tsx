70a12bb0372724301588ec6247fa3ba34f83219f
Flávio Oliveira <83542246+FLviin@users.noreply.github.com>
2022-05-03 17:06:23 -0300

    att

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="" alt="logo" />
+                <img src="/images/ig.news.svg" alt="logo" />
                 <nav>
                     <a href="#" className={styles.active}>Home</a>
                     <a href="#">Posts</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
+import Head  from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
-    <title>IG News</title>
-    <h1>
-      Hello World
-    </h1>
+    <Head>
+     <title>Home |IG News</title>
+    </Head>
+    
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+      <span>👏 Hey, Welcome</span>
+      <h1>News about the <span>React</span> world.</h1>
+      <p>
+        Get access to all the publications <br/>
+        <span>for $9.90 month</span>
+      </p>
+      <SubscribeButton />
+      </section>
+      <img src="/images/Mulher.svg" alt="Girl coding" />
+    </main>
+    
     </>
     
   )
